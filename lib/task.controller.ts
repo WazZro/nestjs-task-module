51882416ba5d7c46aaa1f372431796c12d2746f6
Vadim Usinov <lirael@localhost.localdomain>
2020-04-15 09:14:38 -0400

    fix(task-controller): stop task instead start

--- lib/task.controller.ts
+++ lib/task.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { Task } from './common/task';
 import { TaskService } from './task.service';
 
 @Controller('tasks')
@@ -6,17 +7,17 @@ export class TaskController {
   constructor(private service: TaskService) {}
 
   @Get()
-  getTasks() {
+  public getTasks(): Task[] {
     return this.service.getTasks();
   }
 
   @Get('/start/:taskName')
-  startTaskByName(@Param('taskName') taskName: string) {
+  public startTaskByName(@Param('taskName') taskName: string): Task {
     return this.service.startTask(taskName);
   }
 
   @Get('/stop/:taskName')
-  stopTaskByName(@Param('taskName') taskName: string) {
-    return this.service.startTask(taskName);
+  public stopTaskByName(@Param('taskName') taskName: string): Task {
+    return this.service.stopTask(taskName);
   }
 }
